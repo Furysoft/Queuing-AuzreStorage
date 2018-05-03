5f2de2344c7d91f054c53f656d15c5cc5074c7bc
Unknown <simongparamore@gmail.com>
2018-05-03 07:49:46 +0100

    Project setup

---
+++ src/Component/Furysoft.Queuing.AzureStorage/Interfaces/IQueueWrapper.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IQueueWrapper.cs" company="Simon Paramore">
+// © 2017, Simon Paramore
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Furysoft.Queuing.AzureStorage.Interfaces
+{
+    /// <summary>
+    /// The Queue Wrapper Interface
+    /// </summary>
+    internal interface IQueueWrapper
+    {
+    }
+}
---
+++ src/Component/Furysoft.Queuing.AzureStorage/Logic/QueueWrapper.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QueueWrapper.cs" company="Simon Paramore">
+// © 2017, Simon Paramore
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Furysoft.Queuing.AzureStorage.Logic
+{
+    using Interfaces;
+    using JetBrains.Annotations;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Queue;
+
+    /// <summary>
+    /// The Queue Wrapper
+    /// </summary>
+    public sealed class QueueWrapper : IQueueWrapper
+    {
+        /// <summary>
+        /// The cloud queue client
+        /// </summary>
+        [NotNull]
+        private readonly CloudQueue cloudQueue;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueueWrapper" /> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="queueName">Name of the queue.</param>
+        public QueueWrapper([NotNull] string connectionString, [NotNull] string queueName)
+        {
+            var cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+
+            var queueClient = cloudStorageAccount.CreateCloudQueueClient();
+
+            this.cloudQueue = queueClient.GetQueueReference(queueName);
+        }
+    }
+}
